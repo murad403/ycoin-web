518997bbde4998ba5cda7b3f8f700af643f4d2b5
murad403 <mdm78774@gmail.com>
2026-08-05 12:21:11 +0600

    feat: initialize core landing page structure with new components and global styles

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -1,8 +1,23 @@
+import Culture from '@/components/app/Culture'
+import Hero from '@/components/app/Hero'
+import Instructions from '@/components/app/Instructions'
+import Philosophy from '@/components/app/Philosophy'
+import Roadmap from '@/components/app/Roadmap'
+import Footer from '@/components/shared/Footer'
+import Navbar from '@/components/shared/Navbar'
 import React from 'react'
 
 const page = () => {
   return (
-    <div>page</div>
+    <div>
+      <Navbar />
+      <Hero />
+      <Instructions />
+      <Philosophy />
+      <Roadmap />
+      <Culture />
+      <Footer />
+    </div>
   )
 }
 
