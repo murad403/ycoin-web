efed55d4c97c4187155a9a3e4d5b533ea490fdb5
murad403 <mdm78774@gmail.com>
2026-08-05 14:37:55 +0600

    feat: initialize app layout and main dashboard page with core components

--- src/app/(app)/page.tsx
+++ src/app/(app)/page.tsx
@@ -3,20 +3,15 @@ import Hero from '@/components/app/Hero'
 import Instructions from '@/components/app/Instructions'
 import Philosophy from '@/components/app/Philosophy'
 import Roadmap from '@/components/app/Roadmap'
-import Footer from '@/components/shared/Footer'
-import Navbar from '@/components/shared/Navbar'
-import React from 'react'
 
 const page = () => {
   return (
     <div>
-      <Navbar />
       <Hero />
       <Instructions />
       <Philosophy />
       <Roadmap />
       <Culture />
-      <Footer />
     </div>
   )
 }
